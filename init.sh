a7e12387e9e92a5a0d145a2e58540972e85f5692
julianmasteria <julianmasteria@gmail.com>
2025-07-31 13:51:48 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 FROM n8nio/n8n:latest
+
 COPY init.sh /init.sh
+
+# ✅ Debug: vemos si el archivo existe en esta capa
+RUN ls -l /init.sh && head -n 5 /init.sh
+
 RUN chmod 755 /init.sh
 ENTRYPOINT ["/init.sh"]
