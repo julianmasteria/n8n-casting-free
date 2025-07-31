7b3c5a508b721253156d5f873ba91f5f07a7f79e
julianmasteria <julianmasteria@gmail.com>
2025-07-31 10:31:03 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export DB_SQLITE_DATA_DIRECTORY="/data"
 export N8N_BASIC_AUTH_ACTIVE=true
 export N8N_BASIC_AUTH_USER=admin
-export N8N_BASIC_AUTH_PASSWORD=changeme
-./entrypoint.sh
+export N8N_BASIC_AUTH_PASSWORD=admin
+export N8N_HOST=0.0.0.0
+export N8N_PORT=5678
+export N8N_PROTOCOL=http
+n8n start
