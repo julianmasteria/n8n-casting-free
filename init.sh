32ab2c16a1b98676d414f4e79a9ff7b77128b505
julianmasteria <julianmasteria@gmail.com>
2025-07-31 10:42:41 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-export DB_SQLITE_DATA_DIRECTORY="/data"
+#!/bin/sh
+# Ejemplo mínimo; exporta lo que necesites y arranca n8n
+export DB_SQLITE_PATH=/data/database.sqlite
 export N8N_BASIC_AUTH_ACTIVE=true
 export N8N_BASIC_AUTH_USER=admin
-export N8N_BASIC_AUTH_PASSWORD=admin
-export N8N_HOST=0.0.0.0
-export N8N_PORT=5678
-export N8N_PROTOCOL=http
-n8n start
+export N8N_BASIC_AUTH_PASSWORD=changeme
+n8n
